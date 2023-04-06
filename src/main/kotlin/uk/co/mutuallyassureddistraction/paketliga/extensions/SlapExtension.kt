ca13a838da2a0f5846788bc0cd73af936c23b32f
christiantowb <xtian.zlx@gmail.com>
2023-04-06 22:50:20 +0100

    Added sample extension. change main, and add .env file

---
+++ src/main/kotlin/uk/co/mutuallyassureddistraction/paketliga/extensions/SlapExtension.kt
@@ -0,0 +1,22 @@
+package uk.co.mutuallyassureddistraction.paketliga.extensions
+
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.chatCommand
+import com.kotlindiscord.kord.extensions.utils.respond
+
+class SlapExtension() : Extension() {
+    override val name = "slapExtension"
+
+    override suspend fun setup() {
+        chatCommand {
+            name = "slap"
+            description = "Get slapped!"
+
+            action {
+                message.respond(
+                    "*slaps you with a large, smelly trout!*"
+                )
+            }
+        }
+    }
+}

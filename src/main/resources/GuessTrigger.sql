90274ebbbbea1a0e2b968286c121294173c74fe5
christiantowb <xtian.zlx@gmail.com>
2023-05-06 20:33:22 +0100

    Added Trigger for checking inserts to Guess table

---
+++ src/main/resources/GuessTrigger.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION check_gameid_and_guesstime()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM game WHERE gameid = NEW.gameid) THEN
+        RAISE EXCEPTION 'Game ID % does not exist.', NEW.gameid USING ERRCODE = 'ERRA0';
+    END IF;
+    IF NOT (NEW.guesstime BETWEEN (SELECT windowstart FROM game WHERE gameid = NEW.gameid) AND (SELECT windowclose FROM game WHERE gameid = NEW.gameid)) THEN
+        RAISE EXCEPTION 'Guess time % is not between start and closing window range of the game', NEW.guesstime USING ERRCODE = 'ERRA1';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_gameid_and_guesstime_trigger
+BEFORE INSERT ON GUESS
+FOR EACH ROW
+EXECUTE FUNCTION check_gameid_and_guesstime();
